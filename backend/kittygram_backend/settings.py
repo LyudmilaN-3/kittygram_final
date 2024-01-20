bd21402103cf5a9a5eb83ff3a06962ec2fb084ce
РР›СЋРґРјРёР»Р° РќРёР¶РµРіРѕСЂРѕРґРѕРІР° <lyudmilanizhegorodova@yandex.ru>
2024-01-20 21:16:21 +0300

    AH

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='...')
 
 DEBUG = os.getenv('DEBUG', '...').lower() == 'true'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 INSTALLED_APPS = [
